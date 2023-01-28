014a2daa78f070096989312eefb30912fa283af1
Khanisic <skhan852000@gmail.com>
2023-01-28 16:36:12 +0300

    Readme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,10 @@ const App = () => (
   <BrowserRouter basename='/'>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <div className='flex gap-3 items-center'>
+          <img src={logo} alt="logo" className="w-10 object-contain" />
+          <p className=''>Khanisic Open AI x Dall-E</p>
+        </div>
       </Link>
 
       <Link to="/create-post" className="font-inter font-medium bg-[#10b2b2] text-white px-4 py-2 rounded-md">Create</Link>
